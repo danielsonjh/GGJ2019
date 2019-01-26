d0d3deb2b3798e9bbb86af4f170b777b4d36db5f
Daniel Son <danielsonjh@hotmail.com>
2019-01-26 09:47:53 -0800

    Added project + github for unity

---
+++ GGJ2019/Assets/ResourceManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class ResourceManager : MonoBehaviour
+{
+    public int honey;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
